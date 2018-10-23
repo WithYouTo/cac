35e80970fd0e358ba277b203292d1f09cf276f4c
王天阳 <wty1810862@dingtalk.com>
2018-10-23 09:33:08 +0800

    feat()新增统一更新新增人、新增时间、修改人、修改时间的工具方法

---
+++ src/main/java/com/qcap/cac/tools/EntityTools.java
@@ -0,0 +1,34 @@
+package com.qcap.cac.tools;
+
+import java.util.Date;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import com.qcap.core.utils.AppUtils;
+
+public class EntityTools {
+
+	public static <T> T setCreateEmpAndTime(T t) {
+		if (null == t) {
+			return t;
+		}
+		try {
+			PropertyUtils.setProperty(t, "createEmp", AppUtils.getLoginUserAccount());
+			PropertyUtils.setProperty(t, "createDate", new Date());
+		} catch (Exception e) {
+		}
+		return t;
+	}
+
+	public static <T> T setUpdateEmpAndTime(T t) {
+		if (null == t) {
+			return t;
+		}
+		try {
+			PropertyUtils.setProperty(t, "updateEmp", AppUtils.getLoginUserAccount());
+			PropertyUtils.setProperty(t, "updateDate", new Date());
+		} catch (Exception e) {
+		}
+		return t;
+	}
+}
