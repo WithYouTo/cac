69e876d45e70cc86adddcf3ddb6b703948910856
王天阳 <wty1810862@dingtalk.com>
2018-10-24 18:12:44 +0800

    feat()新增获取区域、获取通用代码接口

--- src/main/java/com/qcap/cac/dao/CommonRestMapper.java
+++ src/main/java/com/qcap/cac/dao/CommonRestMapper.java
@@ -1,11 +1,27 @@
 package com.qcap.cac.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qcap.cac.dto.GetAreaReq;
+import com.qcap.cac.dto.GetAreaResp;
+import com.qcap.cac.dto.GetPubCodeReq;
+import com.qcap.cac.dto.GetPubCodeResp;
+import com.qcap.cac.entity.TbAreaPosition;
 import com.qcap.core.entity.TbManager;
 
 @Repository
 public interface CommonRestMapper extends BaseMapper<TbManager> {
+
+	public List<GetPubCodeResp> getPubCodeList(@Param("getPubCodeReq") GetPubCodeReq req);
+
+	public List<GetAreaResp> getAreaList(@Param("getAreaReq") GetAreaReq req);
+
+	public TbAreaPosition getPositionByPositionCode(String positionCode);
+
+	public List<GetAreaResp> getAreaListByAreaCodeList(List<String> areaCodeList);
 
 }
--- src/main/java/com/qcap/cac/dto/GetAreaResp.java
+++ src/main/java/com/qcap/cac/dto/GetAreaResp.java
@@ -1,28 +1,60 @@
 package com.qcap.cac.dto;
 
-import javax.validation.constraints.NotBlank;
-
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel
 public class GetAreaResp {
 
-	@NotBlank(message = "岗位编号不能为空")
-	@ApiModelProperty(value = "岗位编号")
-	private String positionCode;
+	private String areaId;
+	private String areaCode;
+	private String areaName;
+	private String areaType;
+	private String superAreaCode;
 
-	public String getPositionCode() {
-		return positionCode;
+	public String getAreaId() {
+		return areaId;
 	}
 
-	public void setPositionCode(String positionCode) {
-		this.positionCode = positionCode;
+	public void setAreaId(String areaId) {
+		this.areaId = areaId;
+	}
+
+	public String getAreaCode() {
+		return areaCode;
+	}
+
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+
+	public String getAreaName() {
+		return areaName;
+	}
+
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+
+	public String getAreaType() {
+		return areaType;
+	}
+
+	public void setAreaType(String areaType) {
+		this.areaType = areaType;
+	}
+
+	public String getSuperAreaCode() {
+		return superAreaCode;
+	}
+
+	public void setSuperAreaCode(String superAreaCode) {
+		this.superAreaCode = superAreaCode;
 	}
 
 	@Override
 	public String toString() {
-		return "GetAreaReq [positionCode=" + positionCode + "]";
+		return "GetAreaResp [areaId=" + areaId + ", areaCode=" + areaCode + ", areaName=" + areaName + ", areaType="
+				+ areaType + ", superAreaCode=" + superAreaCode + "]";
 	}
 
 }
